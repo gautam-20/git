fd39bed20f0839f7ea261bda1118bcad52796c6b
gautam-20 <gautamonly2002@gmail.com>
2024-03-20 02:05:05 +0530

    Completed Chapter 2

---
+++ comment.cpp
@@ -0,0 +1,10 @@
+/*
+touch chapter1.txt
+git init
+git status
+git add chapter1.txt
+git commit -m "Complete Chapter 1"
+(for first timer)
+git config --global user.name "gautam-20"
+git config --global user.email gautamonly2002@gmail.com
+*/
